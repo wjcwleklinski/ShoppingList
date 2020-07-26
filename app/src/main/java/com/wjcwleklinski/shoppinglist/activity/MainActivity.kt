0f3b7247481a62d571716f460ac2e50c72033bce
Wojciech Wlekliński <wjcwleklinski@gmail.com>
2020-07-26 18:55:51 +0200

    list-creation | Dialog to provide list name added

--- app/src/main/java/com/wjcwleklinski/shoppinglist/activity/MainActivity.kt
+++ app/src/main/java/com/wjcwleklinski/shoppinglist/activity/MainActivity.kt
@@ -1,38 +1,55 @@
 package com.wjcwleklinski.shoppinglist.activity
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.DialogFragment
+import android.widget.EditText
 import com.wjcwleklinski.shoppinglist.R
-import com.wjcwleklinski.shoppinglist.dialog.CreateListDialogFragment
 
-class MainActivity : AppCompatActivity(), CreateListDialogFragment.CreateListDialogCallbackListener {
+class MainActivity : AppCompatActivity(){
 
     private final var TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "from debug")
     }
 
-    fun createListEvent(view: View) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        val dialogFragment = CreateListDialogFragment()
-        dialogFragment.show(fragmentTransaction, TAG)
 
-    }
-
-    fun startShowListActivity(listName: String) {
-        val intent = Intent(this, CreateListActivity::class.java)
+    private fun startShowListActivity(listName: String) {
+        val intent = Intent(this, ListActivity::class.java).apply {
+            putExtra("List title", listName)
+        }
         startActivity(intent)
     }
 
-    override fun onPositive(listName: String) {
-        Log.i(TAG, listName)
+
+    // Create list onClick method
+    fun createListEventPrompt(view: View) {
+        val layoutInflater = LayoutInflater.from(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_create_list, null)
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setView(dialogView)
+
+        val userInput = dialogView.findViewById<EditText>(R.id.list_title)
+
+        alertDialogBuilder
+            .setCancelable(false)
+            .setPositiveButton("Confirm", DialogInterface.OnClickListener { dialog, which ->
+                Log.d(TAG, userInput.text.toString())
+                startShowListActivity(userInput.text.toString())
+            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                dialog.cancel()
+            })
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
-
-
 }
